0a28a247e5bc69dd13c767a7207ca55accecaa2e
Dan Ben David <bddan013@gmail.com>
2023-12-01 21:48:05 +0200

    adding logs and missing func

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from python_print_color import *
 
 app = FastAPI()
 
@@ -21,7 +22,7 @@ async def login_page(request: Request):
 @app.post("/", response_class=HTMLResponse)
 async def create_user(request: Request, username: str = Form(...)):
     # Check if user exists (in a real app, you would perform database queries here)
-    if not user_exists(username):
+    if not is_user_exists(username):
         # Redirect to page 2
         return templates.TemplateResponse("page2.html", {"request": request, "username": username})
     else:
@@ -39,14 +40,24 @@ async def send_text(request: Request, text: str = Form(...)):
     return templates.TemplateResponse("result.html", {"request": request, "processed_text": processed_text})
 
 
+user_dict = {}
 # Dummy functions for simulating database queries
-def user_exists(username: str):
-    # In a real app, you would check the database for user existence
-    # This is just a dummy function
-    return False
+def is_user_exists(username: str):
+    username = username.strip()
+    
+    def append_user(username):
+        user_dict[username] = ["", ""]
+        print(f"***** User added - {print_colors['yellow']}{username}{print_colors['reset']} *****")
+        return False
+    
+    def error_message(username):
+        print(f"***** {print_colors['red']}MINOR-ERROR:{print_colors['reset']} User exists - {print_colors['yellow']}{username}{print_colors['reset']} *****")
+        return True
+    
+    return append_user(username) if username not in user_dict else error_message(username)
 
 
 def process_text(text: str):
-    # In a real app, you might perform some processing on the text
-    # This is just a dummy function
+    username = 'todo add user to function'
+    print(f"***** {print_colors['yellow']}{username}{print_colors['reset']} added new prompt was given - {print_colors['yellow']}{text}{print_colors['reset']} *****")
     return f"Processed Text: {text}"
